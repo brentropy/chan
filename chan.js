0d7d03d551f321d8a3a922fb119ccc7cdf81c05d
Brent Burgoyne <brent47@gmail.com>
2013-11-28 12:14:13 -0700

    Fixed jshint errors in refactor and a few code style tweaks

--- chan.js
+++ chan.js
@@ -1,9 +1,7 @@
+'use strict';
 
-/**
- * Expose `make()`.
- */
-
-module.exports = make;
+var make
+  , Channel;
 
 /**
  * Make a channel.
@@ -12,34 +10,36 @@ module.exports = make;
  * @api public
  */
 
-function make() {
-  var chan = new Channel;
+make = function make() {
+  var chan = new Channel();
 
   return function(a, b) {
     // yielded
-    if ('function' == typeof a) {
+    if (typeof a === 'function') {
       chan.get(a);
       return;
     }
 
     // (err, res)
-    if (null == a && null != b) a = b;
+    if (a === null) {
+      a = b;
+    }
 
     // value
     chan.add(a);
-  }
-}
+  };
+};
 
 /**
  * Initialize a `Channel`.
  *
- * @api public
+ * @api priate
  */
 
-function Channel() {
+Channel = function Channel() {
   this.queue = [];
   this.items = [];
-}
+};
 
 /**
  * Get an item with `cb`.
@@ -49,7 +49,7 @@ function Channel() {
  */
 
 Channel.prototype.get = function(cb){
-  if (this.items.length) {
+  if (this.items.length > 0) {
     cb(null, this.items.pop());
   } else {
     this.queue.push(cb);
@@ -64,7 +64,7 @@ Channel.prototype.get = function(cb){
  */
 
 Channel.prototype.add = function(val){
-  if (this.queue.length) {
+  if (this.queue.length > 0) {
     this.call(this.queue.pop(), val);
   } else {
     this.items.push(val);
@@ -88,3 +88,10 @@ Channel.prototype.call = function(cb, val){
     cb(null, val);
   }
 };
+
+/**
+ * Expose `make()`.
+ */
+
+module.exports = make;
+
