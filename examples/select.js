2a90e2239c95c8e684f9a83b4aeb1412c9ec281d
Brent Burgoyne <brent47@gmail.com>
2014-01-04 22:17:54 -0700

    Tweaked select example with random timing of values being added to the selected channels

--- examples/select.js
+++ examples/select.js
@@ -1,32 +1,41 @@
-// require the dependencies
-var chan = require('../chan')
-  , co   = require('co');
+// jshint esnext: true, loopfunc: true
+'use strict';
 
-// make two channels
-var ch1 = chan()
-  , ch2 = chan();
+var chan  = require('../chan')
+  , co    = require('co');
 
 co(function *() {
-  // will block until there is data on either ch1 or ch2,
-  // and will return the channel with data
-  // if data is on both channels, a channel will be selected at random
-  var first = yield chan.select([ch1, ch2]);
-  switch (first) {
-    // channel 1 received data
-    case ch1:
-      // retrieve the message from the channel
-      var val1 = yield ch1;
-      console.log(val1);
-      break;
+  var count = 10
+    , ch1
+    , ch2;
 
-    // channel 1 received data
-    case ch2:
-      // retrieve the message from the channel
-      var val2 = yield ch2;
-      console.log(val2);
-      break;
+  while (count-- > 0) {
+    // macke new channels
+    ch1 = chan();
+    ch2 = chan();
+
+    // add a value on each channel after a random amout of time
+    setTimeout(function() { ch1('ch1'); }, Math.random() * 100 | 0);
+    setTimeout(function() { ch2('ch2'); }, Math.random() * 100 | 0);
+
+    // will block until there is data on either ch1 or ch2,
+    // and will return the channel with data
+    // if data is on both channels, a channel will be selected at random
+    switch (yield chan.select([ch1, ch2])) {
+     
+      // channel 1 received data
+      case ch1:
+        // retrieve the message from the channel
+        console.log(yield ch1);
+        break;
+      
+      // channel 2 received data
+      case ch2:
+        // retrieve the message from the channel
+        console.log(yield ch2);
+        break;
+    }
   }
+
 })();
 
-// put 42 onto channel 1
-ch1(42);
